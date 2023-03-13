2ecb6d67c8e7200af31281c510da4f99b675d228
kylejapkowski <kylejapk@gmail.com>
2023-03-13 16:50:37 -0500

    all changes made

---
+++ app.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+PI = 42;
+
+// var can be reassigned and redeclared, while let can only be reassigned, not redeclared.
+
+// var can be reassigned and redeclared, const can be neither redeclared or reassigned
+
+// let can be reassiged but not redeclared. const can be neither reassigned nor redeclared
+
+//using a variables value within its scope before its redeclared
